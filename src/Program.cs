a13d51db9e8a62f9845b4d8f342d9cc85615af9f
ymx <ymx@users.noreply.github.com>
2016-09-27 00:24:47 +0200

    First working prototype

---
+++ src/Program.cs
@@ -0,0 +1,40 @@
+﻿using FileDownloader;
+using System;
+using System.Threading;
+
+namespace FGet
+{
+    public class Program
+    {
+        private static AutoResetEvent completedEvent = new AutoResetEvent(false);
+        private static int exitCode;
+
+        public static int Main(string[] args)
+        {
+            IFileDownloader fileDownloader = new FileDownloader.FileDownloader();
+            fileDownloader.DownloadFileCompleted += DownloadFileCompleted;
+
+            if (args.Length < 1) return 1;
+            fileDownloader.DownloadFileAsync(new Uri(args[0]), args[1]);
+
+            completedEvent.WaitOne();
+            return exitCode;
+        }
+
+        private static void DownloadFileCompleted(object sender, DownloadFileCompletedArgs eventArgs)
+        {
+            if (eventArgs.State == CompletedState.Succeeded)
+            {
+                exitCode = 0;
+                Console.WriteLine("Download completed");
+            }
+            else if (eventArgs.State == CompletedState.Failed)
+            {
+                exitCode = 2;
+                Console.WriteLine("Download failed");
+            }
+
+            completedEvent.Set();
+        }
+    }
+}
---
+++ tests/ProgramTests.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using Xunit;
+
+namespace FGet.Tests
+{
+    public class ProgramTests
+    {
+        public class Main
+        {
+            [Fact]
+            public void CorrectUrlShouldSucceed()
+            {
+                FileInfo tempFile = new FileInfo(Path.GetTempFileName());
+                int exitCode = Program.Main(new[] { "https://install.avira-update.com/package/oeavira/win/int/avira.exe", tempFile.FullName });
+
+                Assert.True(exitCode == 0);
+                Assert.True(tempFile.Length > 4000000);
+            }
+
+            [Fact]
+            public void MissingUrlShouldReturnExitCode1()
+            {
+                int exitCode = Program.Main(new string [] {});
+                Assert.True(exitCode == 1);
+            }
+        }
+    }
+}
