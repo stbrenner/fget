1926e4c751dd86330956c02e26bc6eacd278fabb
Stephan Brenner <stbrenner@users.noreply.github.com>
2016-10-04 00:22:41 +0200

    Integrated ILMerge

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FGet")]
+[assembly: AssemblyTitle("fget")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FGet")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("fget")]
+[assembly: AssemblyCopyright("Copyright © 2016 Stephan Brenner")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
